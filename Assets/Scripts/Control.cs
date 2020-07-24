5818006995813724065f95faf22cda0348ee882e
R0ma-N <x6wert@gmail.com>
2020-07-24 16:04:33 +0300

    rotate

---
+++ Assets/Scripts/Control.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control : MonoBehaviour
+{
+    Vector3 _mousePosition;
+    [SerializeField] Vector3 _rotation;
+
+    void Start()
+    {
+        //_rotation = new Vector3(1, 1, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            _mousePosition = Input.mousePosition;
+            _mousePosition.z = 10f;
+            transform.position = Camera.main.ScreenToWorldPoint(_mousePosition);
+        }
+
+        transform.Rotate(_rotation);
+    }
+}
---
+++ Assets/Scripts/Follow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] float time;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, time);
+        transform.rotation = target.rotation;
+    }
+}
